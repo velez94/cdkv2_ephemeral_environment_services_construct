2345ba6fdb17709779838c92cc41e12741503d96
alejandro.velez <labvel@VelPro>
2023-09-03 23:40:42 -0500

    refactor: separate imported resources

--- src/custom_service.ts
+++ src/custom_service.ts
@@ -2,14 +2,13 @@ import {  StackProps,Fn } from "aws-cdk-lib";
 import { Construct } from 'constructs';
 //import input from "../environment-properties.json";
 import * as ecs from "aws-cdk-lib/aws-ecs";
-import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
 import * as serviceDiscovery from "aws-cdk-lib/aws-servicediscovery";
-import * as alb from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { ImportResources } from "./import_services";
+
 
 export interface EcsCustomServiceProps extends StackProps {
-    //readonly  stackName: string ;
-    //readonly vpcId: string;
+  
     readonly  listenerPort: number;
     readonly containerPort: number;
     readonly public: boolean;
@@ -20,10 +19,17 @@ export interface EcsCustomServiceProps extends StackProps {
   }
 
 export class Cdkv2EphemeralEnvironmentServices extends Construct {
+  /**
+   * @attribute
+   */
+ 
+  public readonly importedResources?: ImportResources;
+  
   constructor(scope: Construct, id: string, props: EcsCustomServiceProps) {
     super(scope, id);
 
-   const taskSize = setTaskSize();
+    const taskSize = setTaskSize();
+   
 
     function setTaskSize() {
       if (props.instanceInputs.inputs.task_size === "x-small") {
@@ -58,33 +64,17 @@ export class Cdkv2EphemeralEnvironmentServices extends Construct {
         };
       }
     }
+    if (props.environmentOutputs.outputs.VPCId != "Null") {
 
-    // Import VPC
-    const importedVpc = ec2.Vpc.fromLookup(this, "VPCImport", {
-      vpcId: props.environmentOutputs.outputs.VPCId,
-    });
-    
-
-    const importedCluster = ecs.Cluster.fromClusterAttributes(
-      this,
-      "ClusterImport",
-      {
-        clusterName: Fn.importValue( `ECSClusterName-${props.parentEnv}`),//environmentOutputs.outputs.ECSClusterName, // 
-        vpc: importedVpc,
-        securityGroups: JSON.parse(
-            props.environmentOutputs.outputs.ECSClusterSecGrps
-        ),
-      }
-    );
-
-    const importLoadBalancer = alb.ApplicationLoadBalancer.fromApplicationLoadBalancerAttributes(this, "ALB", {
-          vpc: importedVpc,
-          loadBalancerDnsName:  Fn.importValue(`LBDNSName-${props.parentEnv}`),
-          loadBalancerArn: Fn.importValue(`ARNALB-${props.parentEnv}`),
-          securityGroupId: Fn.importValue(`SGALB-${props.parentEnv}`),
-
+      this.importedResources = new ImportResources(this, "ImportResources", 
+              { VPCId: props.environmentOutputs.outputs.VPCId, 
+            parentEnv: props.parentEnv,
+              environmentOutputs: props.environmentOutputs  
+            }
+      )
+       
     }
-    );
+  
     // Service Specifications
     const envVarMap: { [index: string]: string } = {};
     const nameSpace= serviceDiscovery.PrivateDnsNamespace.fromPrivateDnsNamespaceAttributes(this, "NameSpace",
@@ -95,9 +85,9 @@ export class Cdkv2EphemeralEnvironmentServices extends Construct {
       });
 
     const loadBalancedEcsService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
-      cluster: importedCluster,
+      cluster: this.importedResources?.importedCluster,
       assignPublicIp: true,
-      loadBalancer: importLoadBalancer,
+      loadBalancer: this.importedResources?.importedAlb,
       memoryLimitMiB: taskSize.memory,
       serviceName: props.instanceInputs.inputs.name,
       listenerPort: props.instanceInputs.inputs.alb_port,
--- test/custom_service.test.ts
+++ test/custom_service.test.ts
@@ -1,20 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { Cdkv2EphemeralEnvironmentServices } from "../src/custom_service";
+import {env} from "process";
 
 
 const deploymentEnv = {
-  account: '571340586587', //env.CDK_DEFAULT_ACCOUNT,
-  region: 'us-east-2'//env.CDK_DEFAULT_REGION,
+  account: env.CDK_DEPLOY_ACCOUNT ?? env.CDK_DEFAULT_ACCOUNT,  //,
+  region: env.CDK_DEPLOY_REGION ?? env.CDK_DEFAULT_REGION, //
 };
+console.log(deploymentEnv)
 
-test('Service Created', () => {
+
+
+test('StackUnitTest', () => {
   const stack = new cdk.Stack();
   // when
   new Cdkv2EphemeralEnvironmentServices(stack, 'CustomService', {
     stackName: 'CustomService',
     env: deploymentEnv,
-    vpcId: "vpc-079741f85e697bdb2",
     listenerPort: 80,
     containerPort: 80,
     public: true,
@@ -33,7 +36,7 @@ test('Service Created', () => {
     environmentOutputs: { "outputs": {
       "ParentEnv": "cdk-ecs-env-demo",
       "ECSClusterSecGrps": "[]",
-      "VPCId": "vpc-079741f85e697bdb2",
+      "VPCId": "Null",
       "ECSClusterSDNamespace": "ecs-cdk-demo.dev"
     } }
   })
